dae43b85e882954c9e0f42693188577d619bd2f6
fatih40 <baserfatih40@gmail.com>
2021-04-08 12:17:02 +0200

    Finished

--- app/src/main/java/com/fatihbaser/kotlinfragment/MainActivity.kt
+++ app/src/main/java/com/fatihbaser/kotlinfragment/MainActivity.kt
@@ -2,10 +2,29 @@ package com.fatihbaser.kotlinfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // onAttach bir yere baglanma
+    }
+    fun firstFragment(view:View){
+
+        val fragmentManager=supportFragmentManager
+        val fragmnetTransaction=fragmentManager.beginTransaction()
+        val firsfragment=BlankFragment()
+
+        fragmnetTransaction.replace(R.id.frameLayout,firsfragment).commit()
+
+    }
+    fun secondFragment(view:View){
+        val fragmentManager=supportFragmentManager
+        val fragmnetTransaction=fragmentManager.beginTransaction()
+        val secondfragment=BlankFragment2()
+
+        fragmnetTransaction.replace(R.id.frameLayout,secondfragment).commit()
     }
 }
